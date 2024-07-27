60cc4630feb0e9f36cf4a032a96998c57deed197
RoudiOlding <roudiolding040504@gmail.com>
2024-07-27 18:23:48 -0500

    Finissh LECTURE 2

--- Memorize2/ContentView.swift
+++ Memorize2/ContentView.swift
@@ -11,18 +11,54 @@ struct ContentView: View {
 // The "View" thing indicates the contentview to "Behaves like a" view.
     //var i: Int, this are normal properties
     //var s: String
+    let emojis = ["🤨", "💤", "🗿", "🕊️", "🦊", "🎸"]
+    @State var cardCount: Int = 4
     
 // Some view is say to act like view, but anythin view
     var body: some View {
         //this are computed properties
-        let emojis = ["🤨", "💤", "🗿", "🕊️"]
-        HStack{
-            ForEach(emojis.indices, id: \.self){ index in
-                CardView(content: emojis[index])
+        VStack{
+            ScrollView{
+                cards
             }
+            cardCountAdjusters
+        }.padding()
+    }
+    
+    var cardCountAdjusters: some View{
+        HStack{
+            cardRemover
+            Spacer()
+            cardAdder
         }
-        .foregroundColor(.yellow)
-        .padding()
+        .imageScale(.large)
+        .font(.largeTitle)
+    }
+    
+    var cards: some View{
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]){
+            ForEach(0..<cardCount, id: \.self){ index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }.foregroundColor(.yellow)
+    }
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View{
+        Button(action: {
+            cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.fill.badge.minus")
+    }
+    
+    var cardAdder: some View{
+        cardCountAdjuster(by: +1, symbol: "rectangle.stack.fill.badge.plus")
     }
 }
 
